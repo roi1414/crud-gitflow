750bd35300353f4c105978071e7ff3cfe12e794c
roi1414 <164715031+roi1414@users.noreply.github.com>
2026-04-03 14:26:27 -0400

    Initial commit - CRUD funcionando

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Base de datos simulada
+let users = [];
+
+// 🔹 CREATE
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send({ message: "User created", user });
+});
+
+// 🔹 READ
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+// 🔹 UPDATE
+app.put('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!users[id]) {
+    return res.status(404).send("User not found");
+  }
+
+  users[id] = req.body;
+  res.send({ message: "User updated", user: users[id] });
+});
+
+// 🔹 DELETE
+app.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!users[id]) {
+    return res.status(404).send("User not found");
+  }
+
+  users.splice(id, 1);
+  res.send({ message: "User deleted" });
+});
+
+// Servidor
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
