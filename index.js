a388f8733b3b02dad58bbbdd158bb97da972296d
roi1414 <164715031+roi1414@users.noreply.github.com>
2026-04-03 14:44:16 -0400

    Add input validation

--- index.js
+++ index.js
@@ -46,3 +46,6 @@ app.delete('/users/:id', (req, res) => {
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
 });
+if (!req.body.name) {
+  return res.status(400).send("Nombre requerido");
+}
